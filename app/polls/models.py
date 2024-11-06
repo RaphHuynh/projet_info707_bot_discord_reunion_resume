f7a0fb067df4b752bcdda480d24ae9420ae64675
Raphhuynh <raphaelle.huynh@outlook.fr>
2024-11-06 18:00:39 +0100

    fit : bdd sqlite

---
+++ app/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200)
+    id_discord = models.CharField(max_length=200)
+    
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
+
+class Resume(models.Model):
+    id = models.AutoField(primary_key=True)
+    text_resume = models.TextField()
+    text_complete = models.TextField()
+    users = models.ManyToManyField(User)
+    messages = models.ManyToManyField(Message)
