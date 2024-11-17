4289fcbaf8a9bd1de41f72d59c680e773c724034
Samy Amarouche <etude.samy.amarouche@gmail.com>
2024-11-18 00:13:00 +0100

    fix: href to index

--- app/meeting_app/urls.py
+++ app/meeting_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("resume", views.resume, name="resume"),
     path("login", views.login, name="login"),
     path("tutorial", views.tutorial, name="tutorial"),
