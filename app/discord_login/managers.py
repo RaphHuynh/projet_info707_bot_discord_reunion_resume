7a526ded5fa9f9912972859bf56e4ca753d3e90c
Samy Amarouche <etude.samy.amarouche@gmail.com>
2024-11-22 21:15:13 +0100

    fix: mise à jour de la méthode de mise à jour de l'utilisateur Discord pour utiliser une instance et sauvegarder les modifications

--- app/discord_login/managers.py
+++ app/discord_login/managers.py
@@ -15,12 +15,12 @@ class DiscordUserOAuth2Manager(models.UserManager):
         )
 
     def update_discord_user(self, user):
-        return self.update(
-            discord_tag=user["username"] + "#" + user["discriminator"],
-            global_name=user["global_name"],
-            avatar=user["avatar"],
-            public_flags=user["public_flags"],
-            flags=user["flags"],
-            locale=user["locale"],
-            mfa_enabled=user["mfa_enabled"],
-        )
+        user_id = user["id"]
+        discord_user = self.get(id=user_id)
+        discord_user.global_name = user["global_name"]
+        discord_user.avatar = user["avatar"]
+        discord_user.public_flags = user["public_flags"]
+        discord_user.flags = user["flags"]
+        discord_user.locale = user["locale"]
+        discord_user.mfa_enabled = user["mfa_enabled"]
+        discord_user.save()
