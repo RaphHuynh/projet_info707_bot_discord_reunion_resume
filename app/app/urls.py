a1ac4e1df89086bba8113266a0cf85a2ce3cdac0
Samy Amarouche <etude.samy.amarouche@gmail.com>
2024-11-22 10:46:07 +0100

    fix: mise à jour des chemins MEDIA et STATIC dans les paramètres et les URL

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,8 +20,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("meeting_app.urls")),
-    path("oauth2/", include("discord_login.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("", include("meeting_app.urls")),
+        path("oauth2/", include("discord_login.urls")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
