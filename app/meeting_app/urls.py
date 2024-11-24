9f198fb22b7fc4f18af769c53d1c960a39806799
Samy <reseaux.samy.amarouche@gmail.com>
2024-11-24 21:01:40 +0100

    fix: mettre à jour les URL de redirection pour la connexion et la déconnexion Discord

--- app/meeting_app/urls.py
+++ app/meeting_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("home", views.home, name="home"),
-    path("login", views.login, name="login"),
     path("tutorial/", views.tutorial, name="tutorial"),
     path("summaries/", views.summaries, name="summaries"),
     path("my_summaries/", views.my_summaries, name="my_summaries"),
--- app/meeting_app/views.py
+++ app/meeting_app/views.py
@@ -1,87 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Resume
 from .graph import generate_heatmap
-
-import matplotlib
-import matplotlib.colors as mcolors
-
-
-matplotlib.use("Agg")
-
-
-def create_heatmap_image(heatmap, start_date):
-    max_reunions_per_day = np.max(heatmap)
-    # Configuration de la taille et des couleurs
-    fig, ax = plt.subplots(figsize=(36, 6))  # Plus de hauteur pour mieux afficher
-    cmap = mcolors.ListedColormap(
-        ["#f7feff", "#e5f5ff", "#c7e9ff", "#98d6fe", "#50baff"]
-    )
-    bounds = [
-        0,
-        max_reunions_per_day * 0.2,
-        max_reunions_per_day * 0.4,
-        max_reunions_per_day * 0.6,
-        max_reunions_per_day * 0.8,
-        max_reunions_per_day,
-    ]  # Définir les seuils pour chaque niveau de couleur
-    norm = mcolors.BoundaryNorm(bounds, cmap.N)
-
-    # Transposer la matrice pour correspondre à l'ordre visuel (jours en lignes, semaines en colonnes)
-    heatmap = heatmap.T[
-        ::-1
-    ]  # Transposer la matrice et inverser l'ordre vertical (axe 0)
-
-    # Afficher la heatmap
-    cax = ax.imshow(
-        heatmap, cmap=cmap, norm=norm, aspect="equal", interpolation="nearest"
-    )
-
-    # Configuration des axes
-    ax.set_yticks(range(7))  # 7 jours par semaine
-    ax.set_yticklabels(
-        ["Sun", "Sat", "Fri", "Thu", "Wed", "Tue", "Mon"], fontsize=10
-    )  # Jours
-    ax.set_xticks(range(53))  # 53 semaines
-    ax.set_xticklabels(range(1, 54), fontsize=8)  # Numéros des semaines
-
-    # Centrer le graphe
-    ax.set_xlim(-0.5, 52.5)  # Limiter les bords horizontaux pour centrer
-    ax.set_ylim(-0.5, 6.5)  # Limiter les bords verticaux pour centrer
-
-    # Supprimer les grilles et le fond gris
-    ax.grid(False)
-    fig.patch.set_facecolor("white")  # Fond de la figure en blanc
-    ax.set_facecolor("white")  # Fond des axes en blanc
-
-    # Ajouter les contours autour des cases si besoin (désactivé pour un design épuré)
-    for i in range(len(heatmap)):
-        for j in range(len(heatmap[i])):
-            ax.add_patch(
-                plt.Rectangle(
-                    (j - 0.5, i - 0.5), 1, 1, fill=False, edgecolor="white", lw=5
-                )
-            )
-
-    # Placer le titre
-    ax.set_title(f"({start_date.year})", fontsize=14, pad=20)
-
-    # Supprimer les bordures inutiles
-    ax.spines[:].set_visible(False)
-    ax.tick_params(left=False, top=False, bottom=False, right=False)
-
-    # Sauvegarder l'image dans un buffer
-    buffer = BytesIO()
-    plt.savefig(buffer, format="png", bbox_inches="tight", dpi=300)
-    buffer.seek(0)
-    image_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    buffer.close()
-    plt.close(fig)
-
-    return f"data:image/png;base64,{image_base64}"
+from django.contrib.auth.decorators import user_passes_test
 
 
 def home(request):
@@ -100,19 +20,17 @@ def home(request):
     return render(request, "home/home.html", {"recent_summaries": recent_summaries})
 
 
-def login(request):
-    return render(request, "logging/login.html")
-
-
 def tutorial(request):
     return render(request, "tutorial/tutorial.html")
 
 
+@user_passes_test(test_func=lambda u: u.is_authenticated, login_url="/oauth2/login")
 def my_summaries(request):
     user_summaries = Resume.objects.filter(attendees=request.user)
     return render(request, "summaries/summaries.html", {"summaries": user_summaries})
 
 
+@user_passes_test(test_func=lambda u: u.is_authenticated, login_url="/oauth2/login")
 def profile(request):
     my_summaries = Resume.objects.filter(attendees=request.user)
     total_meetings = my_summaries.count()
@@ -147,7 +65,7 @@ def profile(request):
         },
     )
 
-
+@user_passes_test(test_func=lambda u: u.is_authenticated, login_url="/oauth2/login")
 def summaries(request):
     my_summaries = Resume.objects.all()
     return render(
@@ -159,4 +77,7 @@ def summaries(request):
 
 def summary(request, id):
     summary = Resume.objects.get(id=id)
+    if request.user not in summary.attendees.all():
+        return redirect("/summaries/")
+
     return render(request, "summaries/summary.html", {"summary": summary})
